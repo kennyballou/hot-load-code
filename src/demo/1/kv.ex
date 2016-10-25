0afd61675e76eef599a76143e154b10d4fc00149
kballou <kballou@devnulllabs.io>
2016-10-24 23:59:55 -0600

    Add first demo module

---
+++ src/demo/1/kv.ex
@@ -0,0 +1,28 @@
+defmodule KV do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def get(key, default \\ nil) do
+    GenServer.call(__MODULE__, {:get, key, default})
+  end
+
+  def put(key, value) do
+    GenServer.call(__MODULE__, {:put, key, value})
+  end
+
+  def handle_call({:get, key, default}, _caller, state) do
+    {:reply, Map.get(state, key, default), state}
+  end
+
+  def handle_call({:put, key, value}, _caller, state) do
+    {:reply, :ok, Map.put(state, key, value)}
+  end
+
+end
